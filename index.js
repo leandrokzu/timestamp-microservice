dcad3afa6863daf9e198c377f4179281a5c91139
Leandro Augusto <leandroaugusto470@gmail.com>
2023-08-17 20:39:16 -0300

    microserviço configurado

--- index.js
+++ index.js
@@ -6,27 +6,35 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+// your first API endpoint...
+app.get('/api/:date?', function (req, res) {
+  let inputDate = req.params.date;
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  if (!isNaN(inputDate)) inputDate = Number(inputDate);
+
+  const date = inputDate ? new Date(inputDate) : new Date();
+  const timestamp = date.getTime();
+
+  console.log(date);
+
+  if (isNaN(timestamp)) return res.status(400).json({ error: 'Invalid Date' });
+
+  res.json({ unix: timestamp, utc: date });
 });
 
-
-
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
